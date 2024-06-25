d581aa989d73ad0941a1cb003bab251239b7d994
Noobowski <p.a.kons@yandex.ru>
2024-06-25 23:23:35 +0300

    Использовал @Autowired с Полем Конструктором Сеттером Через AppConfig

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -1,12 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("duckBean")
 public class Duck5 {
+    private final Egg6 egg;
+
+    @Autowired
+    //Оставил аннотацию, как я понял, если конструктор 1,
+    // то он по умолчанию @Autowired
+    public Duck5(Egg6 egg) {
+        this.egg = egg;
+    }
 
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg.toString();
     }
 }
